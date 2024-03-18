a5d20b68d3bbacabbb1ac999ac218d05603b9e1e
evaabsalyamova <absalyamovaeva@gmail.com>
2024-03-18 18:26:39 +0400

    Use MUI components for `CryptoConverter`

--- src/components/CryptoConverter/index.tsx
+++ src/components/CryptoConverter/index.tsx
@@ -1,24 +1,77 @@
+import {
+  Box,
+  IconButton,
+  MenuItem,
+  Select,
+  Stack,
+  TextField,
+} from "@mui/material";
 import { useState } from "react";
+import SwapVertRoundedIcon from "@mui/icons-material/SwapVertRounded";
 
 const CryptoConverter = () => {
-  const [selectedCrypto, setSelectedCrypto] = useState("ETH");
-  const [inputValue, setInputValue] = useState(0);
+  const [sourceCurrency, setSourceCurrency] = useState("bitcoin");
+  const [targetCurrency, setTargetCurrency] = useState("eth");
+  const [amount, setAmount] = useState("0");
   return (
-    <div>
-      <input
-        type="number"
-        value={inputValue}
-        onChange={(e) => setInputValue(Number(e.target.value))}
-      />
-      <select
-        value={selectedCrypto}
-        onChange={(e) => setSelectedCrypto(e.target.value)}
-      >
-        <option value="USDT">USDT</option>
-        <option value="BTC">BTC</option>
-        <option value="ETH">ETH</option>
-      </select>
-    </div>
+    <Box
+      component="section"
+      sx={{
+        width: 300,
+        height: 400,
+        m: "auto",
+        mt: 10,
+        border: "1px solid grey",
+        borderRadius: 5,
+      }}
+    >
+      <Stack sx={{ gap: 2 }}>
+        <Select
+          value={sourceCurrency}
+          onChange={(e) => setSourceCurrency(e.target.value)}
+        >
+          <MenuItem value="bitcoin">BTC</MenuItem>
+          <MenuItem value="eth">ETH</MenuItem>
+          <MenuItem value="usd">USDT</MenuItem>
+        </Select>
+        <TextField
+          type="number"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+          sx={{
+            "& .MuiOutlinedInput-notchedOutline": {
+              border: "1px solid grey",
+              borderRadius: 5,
+            },
+          }}
+        />
+      </Stack>
+      <IconButton>
+        <SwapVertRoundedIcon />
+      </IconButton>
+      <Stack sx={{ gap: 2 }}>
+        <Select
+          value={targetCurrency}
+          onChange={(e) => setTargetCurrency(e.target.value)}
+        >
+          <MenuItem value="bitcoin">BTC</MenuItem>
+          <MenuItem value="eth">ETH</MenuItem>
+          <MenuItem value="usd">USDT</MenuItem>
+        </Select>
+        <TextField
+          type="number"
+          variant="outlined"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+          sx={{
+            "& .MuiOutlinedInput-notchedOutline": {
+              border: "1px solid grey",
+              borderRadius: 5,
+            },
+          }}
+        />
+      </Stack>
+    </Box>
   );
 };
 
