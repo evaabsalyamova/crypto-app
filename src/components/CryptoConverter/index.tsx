e70730d93bf379df7f96c26fc6bebe7f8e4507e4
evaabsalyamova <absalyamovaeva@gmail.com>
2024-03-19 22:36:07 +0400

    Refactor `CryptoConverter` logic

--- src/components/CryptoConverter/index.tsx
+++ src/components/CryptoConverter/index.tsx
@@ -1,18 +1,97 @@
+import { useCallback, useEffect, useMemo, useState } from "react";
 import {
+  TextField,
+  Select,
+  MenuItem,
+  CircularProgress,
+  Stack,
   Box,
   IconButton,
-  MenuItem,
-  Select,
-  Stack,
-  TextField,
 } from "@mui/material";
-import { useState } from "react";
 import SwapVertRoundedIcon from "@mui/icons-material/SwapVertRounded";
+import useConversion from "../../hooks/useConversion";
+
+enum InputField {
+  FIRST,
+  SECOND,
+}
+
+/**
+ * Доступные типы валют
+ */
+type Currency = "ethereum" | "bitcoin" | "usd";
 
 const CryptoConverter = () => {
-  const [sourceCurrency, setSourceCurrency] = useState("bitcoin");
-  const [targetCurrency, setTargetCurrency] = useState("eth");
-  const [amount, setAmount] = useState("0");
+  // наименование валюты первого поля
+  const [firstFieldCurrency, setFirstFieldCurrency] =
+    useState<Currency>("ethereum");
+  // число вписанное в первое поле
+  const [firstFieldAmount, setFirstFieldAmount] = useState<number>(0);
+  const [secondFieldCurrency, setSecondFieldCurrency] =
+    useState<Currency>("bitcoin");
+  const [secondFieldAmount, setSecondFieldAmount] = useState<number>(0);
+
+  // поле выбранное как "источник" для конвертации
+  const [inputField, setInputField] = useState<InputField>(InputField.FIRST);
+
+  // флаг определящий необходимо ли делать запрос (валюта полей совпадает - не нужно)
+  const enabledToRequest = useMemo(
+    () => firstFieldCurrency !== secondFieldCurrency,
+    [firstFieldCurrency, secondFieldCurrency]
+  );
+
+  const { isLoading, error, convertedAmount } = useConversion({
+    sourceCurrency: firstFieldCurrency,
+    targetCurrency: secondFieldCurrency,
+    amount:
+      inputField === InputField.FIRST ? firstFieldAmount : secondFieldAmount,
+    enabled: enabledToRequest,
+  });
+
+  // эффект в котором следим за результатом конвертации приходящем из хука "useConversion"
+  // определяем какое поле являлось "источником", чтобы вписать результат в противоположное
+  useEffect(() => {
+    if (!isLoading && convertedAmount) {
+      // если источником было второе поле, то результат вписываем в первое поле
+      if (inputField === InputField.SECOND) {
+        setFirstFieldAmount(
+          enabledToRequest ? convertedAmount : secondFieldAmount
+        );
+        return;
+      }
+
+      setSecondFieldAmount(
+        enabledToRequest ? convertedAmount : firstFieldAmount
+      );
+    }
+  }, [
+    firstFieldAmount,
+    secondFieldAmount,
+    convertedAmount,
+    inputField,
+    isLoading,
+    firstFieldCurrency,
+    secondFieldCurrency,
+    enabledToRequest,
+  ]);
+
+  // функция "меняющая" поля и их значения местами
+  const switchSides = useCallback(() => {
+    setFirstFieldAmount(secondFieldAmount);
+    setSecondFieldAmount(firstFieldAmount);
+    setFirstFieldCurrency(secondFieldCurrency);
+    setSecondFieldCurrency(firstFieldCurrency);
+  }, [
+    firstFieldAmount,
+    secondFieldAmount,
+    firstFieldCurrency,
+    secondFieldCurrency,
+  ]);
+
+  if (error) {
+    return <>{error}</>;
+  }
+
   return (
     <Box
       component="section"
@@ -27,17 +106,23 @@ const CryptoConverter = () => {
     >
       <Stack sx={{ gap: 2 }}>
         <Select
-          value={sourceCurrency}
-          onChange={(e) => setSourceCurrency(e.target.value)}
+          value={firstFieldCurrency}
+          onChange={(e) => {
+            setFirstFieldCurrency(e.target.value as Currency);
+          }}
         >
           <MenuItem value="bitcoin">BTC</MenuItem>
-          <MenuItem value="eth">ETH</MenuItem>
+          <MenuItem value="ethereum">ETH</MenuItem>
           <MenuItem value="usd">USDT</MenuItem>
         </Select>
         <TextField
           type="number"
-          value={amount}
-          onChange={(e) => setAmount(e.target.value)}
+          value={firstFieldAmount}
+          onChange={(e) => {
+            // после внесения изменений в поле, необходимо пометить поле как "источник" и записать значение
+            setInputField(InputField.FIRST);
+            setFirstFieldAmount(Number(e.target.value));
+          }}
           sx={{
             "& .MuiOutlinedInput-notchedOutline": {
               border: "1px solid grey",
@@ -46,23 +131,36 @@ const CryptoConverter = () => {
           }}
         />
       </Stack>
-      <IconButton>
-        <SwapVertRoundedIcon />
-      </IconButton>
+      {isLoading ? (
+        <Stack alignItems="center" justifyContent="center" flex={1}>
+          <CircularProgress />
+        </Stack>
+      ) : (
+        <IconButton onClick={switchSides}>
+          <SwapVertRoundedIcon />
+        </IconButton>
+      )}
+
       <Stack sx={{ gap: 2 }}>
         <Select
-          value={targetCurrency}
-          onChange={(e) => setTargetCurrency(e.target.value)}
+          value={secondFieldCurrency}
+          onChange={(e) => {
+            // as Currency тк мы уверены что другого value быть не может
+            setSecondFieldCurrency(e.target.value as Currency);
+          }}
         >
           <MenuItem value="bitcoin">BTC</MenuItem>
-          <MenuItem value="eth">ETH</MenuItem>
+          <MenuItem value="ethereum">ETH</MenuItem>
           <MenuItem value="usd">USDT</MenuItem>
         </Select>
         <TextField
           type="number"
           variant="outlined"
-          value={amount}
-          onChange={(e) => setAmount(e.target.value)}
+          value={secondFieldAmount}
+          onChange={(e) => {
+            setInputField(InputField.SECOND);
+            setSecondFieldAmount(Number(e.target.value));
+          }}
           sx={{
             "& .MuiOutlinedInput-notchedOutline": {
               border: "1px solid grey",
--- src/hooks/useConversion.ts
+++ src/hooks/useConversion.ts
@@ -1,7 +1,6 @@
 import { useEffect, useMemo, useState } from "react";
 import { toast } from "react-toastify";
 
-
 interface IParams {
   sourceCurrency: string;
   targetCurrency: string;
@@ -12,13 +11,21 @@ interface IParams {
 interface IResult {
   isLoading: boolean;
   error: null | string;
-  convertedAmount: string;
+  convertedAmount: number;
 }
 
 const API_KEY = "CG-vXqMf3GPnCRXSrD2VQUPV1Dt";
 
+/**
+ * валидные пары по используемому API
+ */
 const validPairs = ["bitcoin-ethereum", "bitcoin-usd", "ethereum-usd"];
 
+/**
+ * функция которая позволяет перевести пару источник - таргет
+ * в валидный для API формат.
+ * проверяет валидная ли пара изначально, если нет то переворачивает её
+ */
 const getValidatedPairIds = (
   sourceCurrency: string,
   targetCurrency: string
@@ -28,16 +35,23 @@ const getValidatedPairIds = (
   if (validPairs.includes(pair)) {
     return {
       sourceId: sourceCurrency,
+      // когда "ethereum" на втором месте его всегда нужно заменять на "eth"
       targetId: targetCurrency === "ethereum" ? "eth" : targetCurrency,
     };
   }
 
   return {
     sourceId: targetCurrency,
+    // когда "ethereum" на втором месте его всегда нужно заменять на "eth"
     targetId: sourceCurrency === "ethereum" ? "eth" : sourceCurrency,
   };
 };
 
+/**
+ * хук который непосредственно производит конвертацию, принимая: источник,конечную валюту,
+ * колличество монет источника,флаг enabled сообщающий о необходимости делать запрос
+ * и возращающий колличество монет в конечной валюте. пример -  передаём usd,rub,10,true вернёт 950
+ */
 const useConversion = ({
   sourceCurrency,
   targetCurrency,
@@ -46,11 +60,17 @@ const useConversion = ({
 }: IParams): IResult => {
   const [convertedAmount, setConvertedAmount] = useState<number>(amount);
 
+  // перевод абревеатуры переданных валют в подходящий для API формат
   const { sourceId, targetId } = getValidatedPairIds(
     sourceCurrency,
     targetCurrency
   );
 
+  /**
+   * тк в используемом API у нас ограниченный пул пар для запроса цены, мы можем запросить
+   * например, только пару "bitcoin-usd", а обратную не можем ("usd-bitcoin"),
+   * нам необходимо понимать когда запрашиваемая пара была "перевёрнута" чтобы сделать верный рассчёт
+   */
   const isReversePair = useMemo(
     () => sourceId !== sourceCurrency,
     [sourceCurrency, sourceId]
@@ -73,8 +93,10 @@ const useConversion = ({
           throw new Error(`HTTP error: Status ${response.status}`);
         }
         const data = await response.json();
+        // получаем цену
         const rate = data[sourceId][targetId];
 
+        // в случае если пара была перевёрнута, то и цену так же переворачиваем
         const validatedRate = isReversePair ? 1 / rate : rate;
 
         setConvertedAmount(Number(amount) * validatedRate);
@@ -93,7 +115,7 @@ const useConversion = ({
   return {
     isLoading,
     error,
-    convertedAmount: convertedAmount.toString(),
+    convertedAmount,
   };
 };
 
